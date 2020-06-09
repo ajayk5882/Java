c547888ef938b7a24a29d69fbc1d65c643781b62
AJAY KUMAR <AJAY KUMAR@AJAY>
2020-06-09 10:19:11 +0530

    New School initial commit

---
+++ src/main/java/com/bta/school/SchoolProjApplication.java
@@ -0,0 +1,13 @@
+package com.bta.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bta/school/SchoolProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bta.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
